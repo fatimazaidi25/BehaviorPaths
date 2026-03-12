bf98336c67993e3ed942ff07752f6f627e0a8265
fatimazaidi25 <133936616+fatimazaidi25@users.noreply.github.com>
2026-03-11 23:47:45 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./behaviorpath_final.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
